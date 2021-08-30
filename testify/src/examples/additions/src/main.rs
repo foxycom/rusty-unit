089143c2c72a46bbabff960b2ad1cd7f4bb5b626
Vsevolod Tymofyeyev <timvseffolod@gmail.com>
2021-08-30 23:16:21 +0200

    Make example having a struct

--- testify/src/examples/additions/src/main.rs
+++ testify/src/examples/additions/src/main.rs
@@ -1,17 +1,25 @@
 fn main() {}
-pub fn hello(y: u8) {
-    let mut x = 3;
-    if y < x {
-        x = y;
-    } else {
-        x = 3;
+
+struct SomeStruct {
+    a: u8,
+    b: u8
+}
+
+impl SomeStruct {
+    pub fn hello(&mut self, y: u8) {
+        if y < self.a {
+            self.b = y;
+        } else {
+            self.a = 3;
+        }
+    }
+
+    pub fn test_reference(&mut self, x: u8) {
+        if x == 3 {
+            self.a = 4;
+        }
     }
 }
-pub fn test_reference(x: u8) {
-    if x == 3 {
-        let a = 4;
-        let x = a;
-    }
-}
+
 #[cfg(test)]
 mod testify_tests {}
